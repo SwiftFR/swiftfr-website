1ac58d1633932f3a3ea23be1ec9039a7dfc867a1
Fares <fares@macbookair.home>
2025-12-02 19:42:42 +0100

    Fix GA4 tag install

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
+            gtag('config', 'G-XZ6D9ZMVK3');
           `}
         </Script>
       </head>
